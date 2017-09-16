4362a4761864e78e73f224c7baa4e997adef1f4c
linnguyen <nguyenvanlin01041994@gmail.com>
2017-09-16 22:49:06 +0700

    Add create user action api

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 	#Api definition
 	namespace :api, defaults: {format: :json}  do
        scope module: :v1 do
-           resources :users, :only => [:show]
+           resources :users, :only => [:show, :create]
        end
 	end
 end
