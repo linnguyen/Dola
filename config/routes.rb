8b62beb76248989cbc7b979bc408b4fa688b92af
linnguyen <nguyenvanlin01041994@gmail.com>
2017-09-17 14:29:24 +0700

    Add update, destroy user action

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 	#Api definition
 	namespace :api, defaults: {format: :json}  do
        scope module: :v1 do
-           resources :users, :only => [:show, :create]
+           resources :users, :only => [:show, :create, :update, :destroy]
        end
 	end
 end
