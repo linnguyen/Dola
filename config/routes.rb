e7f1736c0f236de636b8dbeaf5745ffdd5cc7a99
Ryne <ryne@enclave.vn>
2017-09-20 13:22:30 +0700

    heroku deploy

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 	#Api definition
 	namespace :api, defaults: {format: :json}  do
        scope module: :v1 do
-           resources :users, :only => [:show, :create, :update, :destroy]
+           resources :users
            resources :sessions, :only => [:create, :destroy]
        end
 	end
