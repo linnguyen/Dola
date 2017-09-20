980a537e0ff46599e85d5e3fadb832c6c36bf09c
Ryne <ryne@enclave.vn>
2017-09-20 13:28:20 +0700

    heroku deploy

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,6 +1,6 @@
 class Payment < ApplicationRecord
-	validatates :name, :user_id, :fee, presence: true
-	validatates :fee, numericality: {greater_than_or_equal_to: 0}, presence: true
+	validates :name, :user_id, :fee, presence: true
+	validates :fee, numericality: {greater_than_or_equal_to: 0}, presence: true
 
 	belongs_to :user
 end
