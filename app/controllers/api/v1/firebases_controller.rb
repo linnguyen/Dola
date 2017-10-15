663e6bc44ee2d608bef0d6254b9205e7d2395292
linnguyen <nguyenvanlin01041994@gmail.com>
2017-10-15 20:49:58 +0700

    Add registration token

--- app/controllers/api/v1/firebases_controller.rb
+++ app/controllers/api/v1/firebases_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::FirebasesController < ApplicationController
 
 	def index
 		fcm = FCM.new("AAAAfdAheBM:APA91bGy4_wmaiEbJQ2z8IdSiu7bxor5lwVhwook4whY98ovJZwWqOhEPItO9T-n0ag0B8f_ikJfVOvD3pZ4t3yPz-QbE6tF5FkLm3BlxB-M6VtbnVB53P8SQYlEafqUMXuAY5dkeuYS")
-		registration_ids= ["12", "13"] # an array of one or more client registration tokens
+		registration_ids= ["dBvtdU3VgGQ:APA91bGeiWbIrLjqXdLg6PRhBnU8v0yrWfPmyZbkeIxbUpnkqUkxgHbTbDkKMZhlwbx588t8mKv_WdAUB4Jr87qFk8wH12NMYbdI9RSY72iMhoOC3PARQGLOchCK1cdZVmx-BZxzX_Yw", "13"] # an array of one or more client registration tokens
 		options = {data: {score: "123"}, collapse_key: "updated_score"}
 		response = fcm.send(registration_ids, options)
     end
