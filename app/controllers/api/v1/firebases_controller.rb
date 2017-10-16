7bf34608ff8d0a42a95544488f9174fe5f656858
Ryne <ryne@enclave.vn>
2017-10-16 13:40:04 +0700

    Add Firebase cloud message successfully

--- app/controllers/api/v1/firebases_controller.rb
+++ app/controllers/api/v1/firebases_controller.rb
@@ -1,10 +1,17 @@
 class Api::V1::FirebasesController < ApplicationController
+	require 'fcm'
 	def index
-		require 'fcm'
 		fcm = FCM.new("AAAAfdAheBM:APA91bGy4_wmaiEbJQ2z8IdSiu7bxor5lwVhwook4whY98ovJZwWqOhEPItO9T-n0ag0B8f_ikJfVOvD3pZ4t3yPz-QbE6tF5FkLm3BlxB-M6VtbnVB53P8SQYlEafqUMXuAY5dkeuYS", :timeout => 30)
-        options = {:notification => "Test notification", 
-           :data => {:message => "This is a FCM Topic Message!"}}
-        response = fcm.send_to_topic('global', options)
+		registration_ids = ["fN16U_lDFM8:APA91bHt2yozE8n_1P7f4RajEUML-NKGnQUuAKS1AwfXQdI9qQGM14WytxIzgQWzMLsLHtUThc-X5QJynGdeCbedgH26-hGXFkhS3AnPucp7db1si0f5mHcxjGVSmOO5uVHVbwDquLyc"]
+ 
+        options = {data: {
+        	               message: "I am Data Message" }, 
+                   notification: {
+                                    title: "Message Title", 
+                                    body: "I am Notifications",
+                                    icon: "myicon"}
+        }
+        response = fcm.send(registration_ids, options)
         render json: response
 	end
 end
