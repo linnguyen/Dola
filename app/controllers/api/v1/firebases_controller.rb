585f5c985cdff32b294cf8cfc076537a824d3d0e
Ryne <ryne@enclave.vn>
2017-10-16 09:56:36 +0700

    Heroku deploy

--- app/controllers/api/v1/firebases_controller.rb
+++ app/controllers/api/v1/firebases_controller.rb
@@ -1,10 +1,10 @@
 class Api::V1::FirebasesController < ApplicationController
-    require 'fcm'
-
 	def index
+		require 'fcm'
 		fcm = FCM.new("AAAAfdAheBM:APA91bGy4_wmaiEbJQ2z8IdSiu7bxor5lwVhwook4whY98ovJZwWqOhEPItO9T-n0ag0B8f_ikJfVOvD3pZ4t3yPz-QbE6tF5FkLm3BlxB-M6VtbnVB53P8SQYlEafqUMXuAY5dkeuYS", :timeout => 30)
         options = {:notification => "Test notification", 
            :data => {:message => "This is a FCM Topic Message!"}}
         response = fcm.send_to_topic('global', options)
+        render json: response
 	end
 end
